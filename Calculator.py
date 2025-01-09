bfde9341649ca1b4ef8d4e243b3b0ff06f50f055
Yukti181 <yuktichaudhari18@gmail.com>
2025-01-09 19:00:11 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number:"))
+print(a)
+b=int(input("Enter second number:"))
+print(b)
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("The addition is: ",sum)
+print("The substration is:",sub)
+print("The multiplication is:",mul)
+print("The division is:",div)
